4ca2b6a23f5f94c54e2da11d9ab0790135d6164f
Kazuho1222 <destiny5914@gmail.com>
2023-06-12 13:24:05 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,22 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:items).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :postage_id, :state_id, :shipping_date, :price, :user_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :state
+  belongs_to :shipping_date
+
+
+  validates :name, :description, :category_id, :status_id, :postage_id, :state_id, :shipping_date_id, :price, :user, :image, presence: true
+  validates :category_id, :status_id, :postage_id, :state_id, :shipping_date_id, numericality: {other_than:1, message: "can't be blank"}
 end
