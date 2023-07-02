404aeb2757076d95ffb3fa461081f8669b178148
Kazuho1222 <destiny5914@gmail.com>
2023-07-02 09:33:12 +0900

    payjp実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_top, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -9,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -19,11 +21,26 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :state_id, :city, :address, :building_name, :telephone_number, :item_id, :order_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :state_id, :city, :address, :building_name, :telephone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def move_to_top
+    if current_user == @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :state_id, :city, :address, :building_name, :telephone_number, :order_id, :image
+  attr_accessor :user_id, :item_id, :post_code, :state_id, :city, :address, :building_name, :telephone_number, :order_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :state_id, :city, :address, :order_id
+    validates :user_id, :item_id, :state_id, :city, :address, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください"}
   end
